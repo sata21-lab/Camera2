b64cb4550deb102e7b7ef21d30f4567f45383a20
wanghan <sata21@163.com>
2021-02-09 17:10:53 +0800

    生辰分支

--- app/src/main/java/com/example/camera2/TestActivity.java
+++ app/src/main/java/com/example/camera2/TestActivity.java
@@ -10,5 +10,6 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
+        //deve分支
     }
 }
