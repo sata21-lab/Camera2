e3df9ccafcaa154867b3bf9358c51f7f4e788ec3
wanghan <sata21@163.com>
2021-02-09 17:06:38 +0800

    改动

--- app/src/main/java/com/example/camera2/TestActivity.java
+++ app/src/main/java/com/example/camera2/TestActivity.java
@@ -10,5 +10,6 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
+        //改动
     }
 }
