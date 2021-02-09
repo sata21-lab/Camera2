b90b8aa2d91f5cd5182145f54d4284594e97b750
wanghan <sata21@163.com>
2021-02-09 17:07:58 +0800

    删除改动

--- app/src/main/java/com/example/camera2/TestActivity.java
+++ app/src/main/java/com/example/camera2/TestActivity.java
@@ -10,6 +10,5 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
-        //改动
     }
 }
